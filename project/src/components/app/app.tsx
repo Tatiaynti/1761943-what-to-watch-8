64382804f348556fd608825c70d05a2c755043d0
Tatiaynti <tatianamo1234@mail.ru>
2021-10-04 22:33:48 +0300

    Описание маршрутов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../main-screen/main-screen';
+import MoviePageDetailsScreen from '../movie-page-details/movie-page-details-screen';
+import MoviePageReviewsScreen from '../movie-page-reviews/movie-page-reviews-screen';
+import MoviePageScreen from '../movie-page/movie-page-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import PlayerScreen from '../player-screen/player-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
 
 type AppScreenProps = {
   cardsCount: number;
@@ -9,12 +17,37 @@ type AppScreenProps = {
 
 function App({cardsCount, promoTitle, promoGenre, promoReleaseYear}: AppScreenProps): JSX.Element {
   return (
-    <MainScreen
-      cardsCount={cardsCount}
-      promoTitle={promoTitle}
-      promoGenre={promoGenre}
-      promoReleaseYear={promoReleaseYear}
-    />);
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen
+            cardsCount={cardsCount}
+            promoTitle={promoTitle}
+            promoGenre={promoGenre}
+            promoReleaseYear={promoReleaseYear}
+          />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignInScreen/>
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <MoviePageScreen />
+        </Route>
+        <Route exact path={AppRoute.FilmDetails}>
+          <MoviePageDetailsScreen />
+        </Route>
+        <Route exact path={AppRoute.FilmReviews}>
+          <MoviePageReviewsScreen />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyListScreen />
+        </Route>
+        <Route exact path={AppRoute.Player}>
+          <PlayerScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,10 @@
+export enum AppRoute {
+  SignIn = '/login',
+  AddReview = '/films/:id/review',
+  Film = '/films/:id',
+  FilmDetails = '/films/:id/details',
+  FilmReviews = '/films/:id/reviews',
+  MyList = '/mylist',
+  Player = '/player/:id',
+  Main = '/'
+}
