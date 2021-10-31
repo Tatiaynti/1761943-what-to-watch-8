1c63565e95429c15ccb4a696d7ce1ff66fe26748
Tatiaynti <tatianamo1234@mail.ru>
2021-11-01 00:20:25 +0300

    правка логина

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -15,7 +15,7 @@ const checkAuthAction = (): ThunkActionResult =>
   async (dispatch, _getState, api) => {
     await api.get(APIRoute.Login)
       .then(() => {
-        dispatch(requireAuthorization(AuthorizationStatus.Auth));
+        dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
       });
   };
 
