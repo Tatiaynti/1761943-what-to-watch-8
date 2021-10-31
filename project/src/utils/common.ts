76bed3ea28c5919a0a201e98e2917a8743a6565e
Tatiaynti <tatianamo1234@mail.ru>
2021-10-31 18:10:26 +0300

    правки адаптера

--- project/src/utils/common.ts
+++ project/src/utils/common.ts
@@ -3,22 +3,24 @@ import {Film, FilmFromServerType} from '../types/film';
 
 function adaptToClient(film: FilmFromServerType): Film {
 
-  const adaptedFilm = Object.assign (
-    {},
-    film,
+  const adaptedFilm =
     {
-      'rate': film['rating'],
-      'releaseYear': film['released'],
-      'title': film['name'],
-      'poster': film['poster_image'],
-      'image': film['background_image'],
-      'backgroundColor': film['background_color'],
-      'videoLink': film['video_link'],
-      'preview': film['preview_video_link'],
-      'runtime': film['run_time'],
-      'isFavorite': film['is_favorite'],
-    },
-  );
+      'rate': film.rating,
+      'releaseYear': film.released,
+      'title': film.name,
+      'poster': film.poster_image,
+      'image': film.background_image,
+      'backgroundColor': film.background_color,
+      'videoLink': film.video_link,
+      'preview': film.preview_video_link,
+      'runtime': film.run_time,
+      'isFavorite': film.is_favorite,
+      'id': film.id,
+      'genre': film.genre,
+      'description': film.description,
+      'director': film.director,
+      'starring': film.starring,
+    };
   return adaptedFilm;
 }
 
